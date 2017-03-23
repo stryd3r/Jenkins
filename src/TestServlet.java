9915eba69bf59de2cfc3001207eeeb92100c5d1d
stryd3r <stryd3r_ady@yahoo.com>
2017-03-23 16:58:43 +0200

    test

--- src/TestServlet.java
+++ src/TestServlet.java
@@ -1,8 +1,6 @@
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,10 +11,8 @@ public class TestServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-		ServletContext servletContext = getServletContext();
-		String contextPath = servletContext.getRealPath(File.separator);
 		PrintWriter out = response.getWriter();
-		out.println("<br/>File system context path (in TestServlet): " + contextPath);
+		out.println("works well");
 
 	}
 
